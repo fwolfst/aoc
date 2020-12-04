b6eb14bbba20ca4ffa831cf8bdf941b7519e844c
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-12-04 11:45:00 +0100

    solved sixth puzzle

--- 2020/README.md
+++ 2020/README.md
@@ -2,6 +2,9 @@
 
 ## Notes
 
+So far, all puzzles read their input via `ARGF` (either pipe data or pass 
+file as argument).
+
 ### Puzzle 1
 
 Puzzle #1 input at https://adventofcode.com/2020/day/1/input but need
@@ -27,3 +30,6 @@ When to extract geo-functions?  Have not yet created a single class, but my
 first methods :)
 It feels hacky as in the 90s, but way more evil than back then.
 
+### Puzzle 6
+Would feel good if I refactor a bit first. Did the bare minimum and will not
+understand the solution anymore in t+2h.
--- 2020/p6.rb
+++ 2020/p6.rb
@@ -6,11 +6,12 @@
 # X horizontal, Y vertical
 X = 0
 Y = 1
-STEP = [3,1]
 
-DEBUG = true
+DEBUG = false
 
 def is_tree? map, position, pattern_width
+  return false if position[Y] > map.size
+
   if DEBUG
     puts map[position[Y]]
     x = position[X] % pattern_width
@@ -18,38 +19,58 @@ def is_tree? map, position, pattern_width
       (x).times { putc " " }
     end
     puts "*"
-    puts "tree at #{position[X] % pattern_width}? #{map[position[Y]][position[X] % pattern_width] == '#'}"
+    puts "tree at #{position[X] % pattern_width}, #{position[Y]}? #{map[position[Y]][position[X] % pattern_width] == '#'}"
     puts
   end
   map[position[Y]][position[X] % pattern_width] == '#'
 end
 
+def do_step position, step_pattern
+  position = [position[X] + step_pattern[X], position[Y] + step_pattern[Y]]
+end
+
 map = ARGF.readlines.map {|l| l.strip }
 
-pattern_width = map[0].length
-pattern_height = map.count
+def count_occurrences step_pattern, map
+  puts step_pattern
+  pattern_width = map[0].length
+  pattern_height = map.count
+  puts "map of #{pattern_width} x #{pattern_height}"
 
-def do_step position
-  position = [position[X] + STEP[X], position[Y] + STEP[Y]]
-end
+  position = [0,0]
 
-position = [0,0]
-
-trees_on_path_count = 0
-if is_tree? map, position, pattern_width
-  trees_on_path_count = 1
-end
-
-# count trees
-(pattern_height-1).times do |_num|
-  if is_tree? map, position = do_step(position), pattern_width
-    trees_on_path_count+= 1
+  trees_on_path_count = 0
+  if is_tree? map, position, pattern_width
+    trees_on_path_count = 1
   end
+  
+  # count trees
+  (pattern_height-1).times do |_num|
+    if is_tree? map, position = do_step(position, step_pattern), pattern_width
+      trees_on_path_count+= 1
+    end
+  end
+
+  trees_on_path_count
 end
 
-puts trees_on_path_count
+step_patterns = [
+  [1,1],
+  [3,1],
+  [5,1],
+  [7,1],
+  [1,2],
+]
 
-exit 1
+counts = step_patterns.map do |step_pattern|
+  count_occurrences step_pattern, map
+end
+
+puts counts
+puts counts.reduce(1, :*)
+
+exit 0
+
 if DEBUG
   # puts debug map
   position = [0,0]
