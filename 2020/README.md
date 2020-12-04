0da7a18047172833696359e6cfa9d85707963018
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-12-04 09:29:38 +0100

    solve first puzzle

---
+++ 2020/README.md
@@ -0,0 +1,13 @@
+# AoC 2020
+
+## Notes
+
+### Puzzle 1
+
+Puzzle #1 input at https://adventofcode.com/2020/day/1/input but need
+authentication to download.
+
+I do not yet know how mean the puzzles are, so I assume a friendly input
+(positive integers).
+
+Quite some time pressure, I go without tests and super-iterative brute-force approach.
---
+++ 2020/p1.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+values = ARGF.readlines.map {|l| l.to_i }
+
+searched_pair = []
+
+values.uniq.each_with_index do |first_candidate, index|
+  values[index..-1].each do |second_candidate|
+    if first_candidate + second_candidate == 2020
+      searched_pair = [first_candidate, second_candidate]
+      break
+    end
+  end
+end
+
+puts "#{searched_pair[0]} + #{searched_pair[1]} = #{searched_pair[0] + searched_pair[1]}"
+puts "#{searched_pair[0]} * #{searched_pair[1]} = #{searched_pair[0] * searched_pair[1]}"
+
+exit 0
