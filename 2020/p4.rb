5c17e794dc5703fbdca19ad13fe6f049f3222694
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-12-04 10:25:44 +0100

    solved fourth puzzle

---
+++ 2020/p4.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+
+# Puzzle 3: https://adventofcode.com/2020/day/2#part2
+# Felix Wolfsteller
+
+values = ARGF.readlines.map {|l| [*l.split] }
+
+correct_entries = 0
+
+values.each do |entry|
+  position_range, letter, password = entry
+
+  positions = position_range.split("-").map(&:to_i)
+
+  letter = letter[0]
+
+  if (password[positions[0]-1] == letter) ^ (password[positions[1]-1] == letter)
+    correct_entries+= 1
+  end
+end
+
+puts correct_entries
+
+exit 0
