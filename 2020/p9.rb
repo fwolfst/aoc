8e1b5a665c3f41ca3fe92ce138e83b593d7e497f
Felix Wolfsteller <felix.wolfsteller@gmail.com>
2020-12-05 11:37:04 +0100

    solved ninth puzzle (5/1)

---
+++ 2020/p9.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+# Puzzle 9: https://adventofcode.com/2020/day/5
+# Felix Wolfsteller
+
+DEBUG = false
+
+PLANE_LENGTH = 128
+PLANE_WIDTH = 8
+
+data = ARGF.readlines
+
+seats = data.map do |boarding_pass|
+  row_range_start = 1
+  row_range_end   = PLANE_LENGTH
+  
+  column_range_start = 1
+  column_range_end   = PLANE_WIDTH
+  
+  row_range = (row_range_start..row_range_end)
+  column_range = (column_range_start..column_range_end)
+  
+  boarding_pass[0..6].chars.each_with_index do |char,idx|
+    half_size = row_range.size / 2
+    if char == 'F'
+      row_range_end-= half_size
+    else
+      row_range_start+= half_size
+    end
+    row_range = (row_range_start..row_range_end)
+  end
+  
+  row = row_range.begin
+
+  boarding_pass[7..10].chars.each_with_index do |char,idx|
+    half_size = column_range.size / 2
+    if char == 'L'
+      column_range_end-= half_size
+    else
+      column_range_start+= half_size
+    end
+    column_range = (column_range_start..column_range_end)
+  end
+
+  column = column_range.begin
+
+  [row-1, column-1]
+end
+
+seat_ids = seats.map do |row, column|
+  row * 8 + column
+end
+
+puts seat_ids.max
+
+exit 0
