fa616ac25fa1b5f3fad343c0d9cf0bee03d5dbae
Felix Wolfsteller <felix.wolfsteller@mailbox.org>
2024-12-01 09:20:36 +0100

    2024#01#01

---
+++ 2024/1/src/main.rs
@@ -0,0 +1,37 @@
+use std::fs;
+
+// Learning Rust String API
+//
+// String#split_whitespace (iter)
+// String#lines (iter)
+// String#split
+// String#matches
+//
+// parse -> into i32
+fn main() {
+    let contents = fs::read_to_string("1.input").unwrap();
+    //println!("{contents}");
+
+    let mut nums_left: Vec<i32> = Vec::new();
+    let mut nums_right: Vec<i32> = Vec::new();
+
+    for line in contents.lines() {
+        let mut nums_iter = line.split_whitespace();
+        nums_left.push(nums_iter.next().unwrap().parse().unwrap());
+        nums_right.push(nums_iter.next().unwrap().parse().unwrap());
+    }
+
+    //println!("{:?}", nums_left);
+    nums_left.sort();
+    nums_right.sort();
+
+    assert!(nums_left.len() == nums_right.len());
+
+    let mut i = 0;
+    let mut sum = 0;
+    while i < nums_left.len() {
+        sum += (nums_left[i] - nums_right[i]).abs();
+        i += 1;
+    }
+    println!("{:?}", sum);
+}
