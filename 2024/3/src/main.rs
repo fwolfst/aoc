454c3a06c20278ffc1cf5ab75f0a293998777730
Felix Wolfsteller <felix.wolfsteller@mailbox.org>
2024-12-14 14:40:13 +0100

    add input file name argument to program

--- 2024/3/src/main.rs
+++ 2024/3/src/main.rs
@@ -1,13 +1,26 @@
+use std::env;
 use std::fs;
 use regex::Regex;
 
+// "cargo run samples.txt"
 fn main() {
-    let input = fs::read_to_string("input").unwrap();
+    let args: Vec<String> = env::args().collect();
+    //dbg!(args);
+
+    let input_file = if args.len() > 1 {
+        args[1].clone()
+    } else {
+        "input".to_string()
+    };
+
+    let input = fs::read_to_string(&input_file).unwrap();
     
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let contained_numbers = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    println!("Reading from {:?}", input_file);
 
     let mut r = 0;
-    for (_, [mul_one, mul_two]) in re.captures_iter(&input).map(|c| c.extract()) {
+    for (_, [mul_one, mul_two]) in contained_numbers.captures_iter(&input).map(|c| c.extract()) {
         //println!("{:?} {:?}", mul_one, mul_two);
         let m1: i32 = mul_one.parse().unwrap();
         let m2: i32 = mul_two.parse().unwrap();
@@ -15,6 +28,8 @@ fn main() {
     }
     println!("one:");
     println!("{:?}", r);
+
+
     //println!("two:");
     //println!("{:?}", cnt);
 }
