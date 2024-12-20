f7096591b3ede95e96f693d153580bb4ed3c8c26
Felix Wolfsteller <felix.wolfsteller@mailbox.org>
2024-12-20 21:01:15 +0100

    2024#08#2

--- 2024/8/src/main.rs
+++ 2024/8/src/main.rs
@@ -10,7 +10,7 @@ fn debug_map(map: &Vec<Vec<Vec<char>>>) {
                 print!(".");
             }
             else {
-                print!("#");
+                print!("{}", col.iter().count());//next().unwrap_or(&'?'));
             }
         }
         println!();
@@ -24,7 +24,7 @@ fn debug_map(map: &Vec<Vec<Vec<char>>>) {
 /// let r = 1;
 /// assert_eq!(r, 3); // (will fail)
 /// ```
-fn solve(input: &str) -> i32 {
+fn solve(input: &str) -> (i32, i32) {
     let mut frequency_emitter_locations = HashMap::<char, Vec<(i32,i32)>>::new();
 
     for (y, line) in input.lines().enumerate() {
@@ -45,33 +45,52 @@ fn solve(input: &str) -> i32 {
     let width_range = 0..width;
     let height_range = 0..height;
 
-    let mut antinodes : Vec<Vec<Vec<char>>> = vec![vec![vec![]; width as usize]; height as usize];
+    let mut antinodes1 : Vec<Vec<Vec<char>>> = vec![vec![vec![]; width as usize]; height as usize];
+    let mut antinodes2 : Vec<Vec<Vec<char>>> = vec![vec![vec![]; width as usize]; height as usize];
 
     for (frqncy, positions) in frequency_emitter_locations.iter() {
         // for each combination in positions
         for two_emitters in positions.iter().permutations(2) {
             let (x1,y1) = two_emitters[0];
             let (x2,y2) = two_emitters[1];
-            //((x1,y1),(x2,y2)) = two_emitters
+
+            antinodes2[*y1 as usize][*x1 as usize].push(*frqncy);
+
             let vector = ((x2 - x1), (y2 - y1));
-            // first in one direction, then in other.
+            let mut cursor = (two_emitters[0].0 - vector.0, two_emitters[0].1 - vector.1);
+            while width_range.contains(&cursor.0) && height_range.contains(&cursor.1) {
+                antinodes2[cursor.1 as usize][cursor.0 as usize].push(*frqncy);
+                cursor.0 -= vector.0;
+                cursor.1 -= vector.1;
+            }
+
             // I suspect that there is a possibility of a third and fourth point in between is
             // actually not included in the example on purpose. Or does it need to be outside?
             let antinode = (two_emitters[0].0 - vector.0, two_emitters[0].1 - vector.1);
             if width_range.contains(&antinode.0) && height_range.contains(&antinode.1) {
-                antinodes[antinode.1 as usize][antinode.0 as usize].push(*frqncy);
+                antinodes1[antinode.1 as usize][antinode.0 as usize].push(*frqncy);
             }
         }
     }
 
-    debug_map(&antinodes);
+    debug_map(&antinodes2);
 
-    antinodes
+    let solution1 = antinodes1
         .iter()
         .map(|row| row.iter().filter(|col| col.iter().len() > 0 ).count() as i32)
         .sum::<i32>()
+        ;
         //.map(|row| row.iter().map(|col| col.iter().len()).sum::<usize>())
         //.sum::<usize>() as i32
+
+    let solution2 = antinodes2
+        .iter()
+        .map(|row| row.iter().filter(|col| col.iter().len() > 0 ).count() as i32)
+        .sum::<i32>()
+        ;
+
+
+    (solution1, solution2)
 }
 
 // "cargo run sample"
@@ -86,11 +105,10 @@ fn main() {
 
     let input = fs::read_to_string(input_file).unwrap();
 
-    println!("One: {}", solve(&input));
+    let solutions = solve(&input);
+    println!("One: {}", solutions.0);
 
-    let r: usize = 0;
-    println!("two:");
-    println!("{:?}", r);
+    println!("Two: {}", solutions.1);
 }
 
 // LEARN put your tests here
@@ -116,6 +134,6 @@ mod tests {
             ............"
         };
 
-        assert_eq!(solve(sample), 14);
+        assert_eq!(solve(sample), (14, 34));
     }
 }
